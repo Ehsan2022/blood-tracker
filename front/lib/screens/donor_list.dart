057f6c657a62767733e7c0be8b5d2222bf066ee9
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2025-06-27 17:02:12 +0430

    donor list.dart code added

---
+++ front/lib/screens/donor_list.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import '../models/donor.dart';
+import '../services/api_service.dart';
+import 'donor_form.dart';
+import 'donor_detail.dart';
+
+class DonorListScreen extends StatefulWidget {
+  @override
+  _DonorListScreenState createState() => _DonorListScreenState();
+}
+
+class _DonorListScreenState extends State<DonorListScreen> {
+  late Future<List<Donor>> donors;
+
+  @override
+  void initState() {
+    super.initState();
+    donors = ApiService.fetchDonors();
+  }
+
+  void refresh() {
+    setState(() {
+      donors = ApiService.fetchDonors();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Donors')),
+      body: FutureBuilder<List<Donor>>(
+        future: donors,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
+          return ListView(
+            children: snapshot.data!
+                .map((donor) => Card(
+                      child: ListTile(
+                        title: Text(donor.name),
+                        subtitle: Text('${donor.bloodGroup} - ${donor.city}'),
+                        onTap: () async {
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (_) => DonorDetailScreen(donor: donor)),
+                          );
+                          refresh();
+                        },
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () async {
+                            await ApiService.deleteDonor(donor.id!);
+                            refresh();
+                          },
+                        ),
+                      ),
+                    ))
+                .toList(),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => DonorFormScreen()),
+          );
+          refresh();
+        },
+      ),
+    );
+  }
+}
