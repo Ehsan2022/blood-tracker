c6c0afcdc9a26571ccf5702863e9fb9cb5d2018c
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2025-06-27 17:08:07 +0430

    donation class created

---
+++ front/lib/models/donation.dart
@@ -0,0 +1,35 @@
+class Donation {
+  final int? id;
+  final int donorId;
+  final String date;
+  final String hospital;
+  final int units;
+  final String notes;
+
+  Donation({
+    this.id,
+    required this.donorId,
+    required this.date,
+    required this.hospital,
+    required this.units,
+    required this.notes,
+  });
+
+  factory Donation.fromJson(Map<String, dynamic> json) => Donation(
+        id: json['id'],
+        donorId: json['donor']['id'],
+        date: json['date'],
+        hospital: json['hospital'],
+        units: json['units'],
+        notes: json['notes'] ?? '',
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'date': date,
+        'hospital': hospital,
+        'units': units,
+        'notes': notes,
+        'donor': {'id': donorId}
+      };
+}
