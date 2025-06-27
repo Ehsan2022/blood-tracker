1b83acbaffff4629fceb000578da4d1d8fccc2bf
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2025-06-27 17:09:51 +0430

    api updated

--- front/lib/services/api_service.dart
+++ front/lib/services/api_service.dart
@@ -1,31 +1,34 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
-import '../models/donor.dart';
 
-class ApiService {
-  static const baseUrl = 'http://10.0.2.2:8080/api';
+import '../models/donation.dart';
 
-  static Future<List<Donor>> fetchDonors() async {
-    final res = await http.get(Uri.parse('$baseUrl/donors'));
-    final data = json.decode(res.body) as List;
-    return data.map((e) => Donor.fromJson(e)).toList();
-  }
+// Inside ApiService class:
 
-  static Future<void> createDonor(Donor donor) async {
-    await http.post(Uri.parse('$baseUrl/donors'),
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode(donor.toJson()),
-    );
-  }
+Future<List<Donation>> fetchDonationsByDonor(int donorId) async {
+  var baseUrl;
+  final res = await http.get(Uri.parse('$baseUrl/donations/donor/$donorId'));
+  final data = json.decode(res.body) as List;
+  return data.map((e) => Donation.fromJson(e)).toList();
+}
 
-  static Future<void> deleteDonor(int id) async {
-    await http.delete(Uri.parse('$baseUrl/donors/$id'));
-  }
+Future<void> createDonation(Donation donation, dynamic baseUrl) async {
+  await http.post(
+    Uri.parse('$baseUrl/donations'),
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode(donation.toJson()),
+  );
+}
 
-  static Future<void> updateDonor(Donor donor) async {
-    await http.put(Uri.parse('$baseUrl/donors/${donor.id}'),
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode(donor.toJson()),
-    );
-  }
+Future<void> updateDonation(Donation donation, dynamic baseUrl) async {
+  await http.put(
+    Uri.parse('$baseUrl/donations/${donation.id}'),
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode(donation.toJson()),
+  );
+}
+
+Future<void> deleteDonation(int id, dynamic baseUrl) async {
+  await http.delete(Uri.parse('$baseUrl/donations/$id'));
 }
