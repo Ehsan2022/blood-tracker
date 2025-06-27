04c265980acfebd996412dc0600dc4108a5d0083
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2025-06-27 17:11:47 +0430

    donation form created

---
+++ front/lib/screens/donation_form.dart
@@ -0,0 +1,72 @@
+import 'package:class_project/services/api_service.dart' as ApiService;
+import 'package:flutter/material.dart';
+import '../models/donation.dart';
+import '../services/api_service.dart';
+
+class DonationFormScreen extends StatefulWidget {
+  final int donorId;
+  final Donation? donation;
+
+  const DonationFormScreen({required this.donorId, this.donation});
+
+  @override
+  State<DonationFormScreen> createState() => _DonationFormScreenState();
+}
+
+class _DonationFormScreenState extends State<DonationFormScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late String date, hospital, notes;
+  late int units;
+
+  @override
+  void initState() {
+    super.initState();
+    final d = widget.donation;
+    date = d?.date ?? '';
+    hospital = d?.hospital ?? '';
+    units = d?.units ?? 1;
+    notes = d?.notes ?? '';
+  }
+
+  void save() async {
+    if (_formKey.currentState!.validate()) {
+      final donation = Donation(
+        id: widget.donation?.id,
+        donorId: widget.donorId,
+        date: date,
+        hospital: hospital,
+        units: units,
+        notes: notes,
+      );
+      if (widget.donation == null) {
+        await ApiService.createDonation(widget.donorId as Donation, donation);
+      } else {
+        await ApiService.updateDonation(widget.donorId as Donation, donation);
+      }
+      Navigator.pop(context, true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.donation == null ? 'Add Donation' : 'Edit Donation')),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(initialValue: date, decoration: InputDecoration(labelText: 'Date (yyyy-mm-dd)'), onChanged: (v) => date = v),
+              TextFormField(initialValue: hospital, decoration: InputDecoration(labelText: 'Hospital'), onChanged: (v) => hospital = v),
+              TextFormField(initialValue: units.toString(), decoration: InputDecoration(labelText: 'Units'), keyboardType: TextInputType.number, onChanged: (v) => units = int.parse(v)),
+              TextFormField(initialValue: notes, decoration: InputDecoration(labelText: 'Notes'), onChanged: (v) => notes = v),
+              SizedBox(height: 20),
+              ElevatedButton(child: Text('Save'), onPressed: save),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
